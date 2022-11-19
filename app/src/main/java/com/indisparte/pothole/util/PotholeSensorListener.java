142645a960f823777122d3222d1430b2fba7581d
Antonio <tonydinu02@gmail.com>
2022-11-19 12:10:47 +0100

    Aggiunto sensore per le buche, fix #3

---
+++ app/src/main/java/com/indisparte/pothole/util/PotholeSensorListener.java
@@ -0,0 +1,86 @@
+package com.indisparte.pothole.util;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.indisparte.pothole.PotholeApplication;
+
+/**
+ * @author Antonio Di Nuzzo (Indisparte)
+ */
+public class PotholeSensorListener implements SensorEventListener {
+    private static PotholeSensorListener instance;
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private static final double ACCELERATION_THRESHOLD = 25.000;//TODO should be customizable
+    private float previous_z_acceleration;
+    private boolean initialized;
+
+    private PotholeSensorListener() {
+    }
+
+    public static PotholeSensorListener getInstance() {
+        if (instance == null)
+            instance = new PotholeSensorListener();
+        return instance;
+    }
+
+    public void start() {
+        try {
+            sensorManager = (SensorManager) PotholeApplication.appContext.getSystemService(Context.SENSOR_SERVICE);
+            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+            if (sensor != null) {
+                sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+                Log.d(PotholeApplication.LOG_TAG, "Acceleration sensor initialized");
+            } else {
+                Log.d(PotholeApplication.LOG_TAG, "Acceleration sensor not found");
+                Toast.makeText(PotholeApplication.appContext, "Acceleration sensor not found", Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (Exception e) {
+            Log.e(PotholeApplication.LOG_TAG, "PotholeSensorListener start: Exception->" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        try {
+            if (sensor != null && sensorManager != null) {
+                sensorManager.unregisterListener(instance);
+                sensorManager = null;
+                sensor = null;
+            }
+        } catch (Exception e) {
+            Log.e(PotholeApplication.LOG_TAG, "PotholeSensorListener stop: Exception-> " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float current_z_value = event.values[2];
+
+        if (!initialized) {
+            previous_z_acceleration = current_z_value;
+            initialized = true;
+        }
+        float deltaZ = Math.abs(previous_z_acceleration - current_z_value);
+
+        if (deltaZ > ACCELERATION_THRESHOLD) {
+            // TODO Pothole found, build a pothole instance
+            Log.d(PotholeApplication.LOG_TAG, "onSensorChanged: deltaZ-> " + deltaZ);
+            Toast.makeText(PotholeApplication.appContext, "Pothole found!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
--- app/src/main/java/com/indisparte/pothole/view/MainActivity.java
+++ app/src/main/java/com/indisparte/pothole/view/MainActivity.java
@@ -1,17 +1,22 @@
-package com.indisparte.pothole;
+package com.indisparte.pothole.view;
 
 
 import android.os.Bundle;
 import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.indisparte.pothole.databinding.ActivityMainBinding;
+import com.indisparte.pothole.util.PotholeSensorListener;
 
 /**
  * @author Antonio Di Nuzzo (Indisparte)
  */
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
+
     private ActivityMainBinding binding;
+    private final PotholeSensorListener potholeSensorListener = PotholeSensorListener.getInstance();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,19 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        potholeSensorListener.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        potholeSensorListener.stop();
     }
 }
