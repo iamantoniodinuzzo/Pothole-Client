bf4ad7207042b536759b7aa16913c2d40cd10466
Antonio <tonydinu02@gmail.com>
2022-11-19 09:40:35 +0100

    Init code
    - Add view and data binding
    - Internet permissions
    - Add Jetpack compose library

---
+++ app/src/main/java/com/indisparte/pothole/MainActivity.java
@@ -0,0 +1,23 @@
+package com.indisparte.pothole;
+
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.indisparte.pothole.databinding.ActivityMainBinding;
+
+/**
+ * @author Antonio Di Nuzzo (Indisparte)
+ */
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+}
