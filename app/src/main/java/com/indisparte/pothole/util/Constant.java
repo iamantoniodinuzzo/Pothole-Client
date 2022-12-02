cbd43ef07f50f0157fdd573a46df043a2c83a5d2
Antonio <tonydinu02@gmail.com>
2022-12-02 15:45:18 +0100

    Fix #17 , Fix #15 , FIx #8 , Fix #7

--- app/src/main/java/com/indisparte/pothole/util/Constant.java
+++ app/src/main/java/com/indisparte/pothole/util/Constant.java
@@ -2,6 +2,9 @@ package com.indisparte.pothole.util;
 
 import android.Manifest;
 
+/**
+ * @author Antonio Di Nuzzo (Indisparte)
+ */
 public class Constant {
     //Location
     public static final int LOCATION_REQUEST_CODE = 1001;
@@ -9,5 +12,22 @@ public class Constant {
     public static final String ACCESS_FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     public static final String ACCESS_COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     public static final String[] permissions = {ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION};
-}
 
+    //service and broadcast
+    public static final int LOCATION_INTERVAL_MILLIS = 4000;
+    public static final int FASTEST_INTERVAL_MILLIS = 200;
+    public static final int LOCATION_SERVICE_ID = 175;
+    public static final String ACTION_START_LOCATION_SERVICE = "startLocationTrackingService";
+    public static final String ACTION_STOP_LOCATION_SERVICE = "stopLocationTrackingService";
+    public static final String PACKAGE_NAME = "com.indisparte.locationapplication";
+    public static final String EXTRA_LOCATION = PACKAGE_NAME + ".location";
+    public static final String ACTION_BROADCAST = PACKAGE_NAME + ".broadcast";
+
+    //Map customization
+    public static final int DEFAULT_RANGE = 100;
+    public static final String DEFAULT_CAMERA_ZOOM = "15f";
+    public static final String ZOOM_PREFERENCE_KEY = "zoom_preference";
+    public static final String PRECISION_RANGE_KEY = "precision_range";
+    public static final String MAP_TYPE_PREFERENCE_KEY = "map_type_preference";
+    public static final String DARK_MODE_PRECISION_KEY = "dark_mode";
+}
---
+++ app/src/main/java/com/indisparte/pothole/view/adapter/BindingAdapter.java
@@ -0,0 +1,74 @@
+package com.indisparte.pothole.view.adapter;
+
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.widget.AppCompatToggleButton;
+import androidx.appcompat.widget.Toolbar;
+
+/**
+ * @author Antonio Di Nuzzo (Indisparte)
+ */
+public class BindingAdapter {
+
+    @androidx.databinding.BindingAdapter(value = {"permission"})
+    public static void shouldShowProgressBar(ProgressBar progressBar,Boolean permission) {
+        if (permission){
+            progressBar.setVisibility(View.GONE);
+        }else {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @androidx.databinding.BindingAdapter(value = {"permission"})
+    public static void shouldShowTrackingButton(AppCompatToggleButton button, Boolean permission) {
+        if (permission){
+            button.setVisibility(View.VISIBLE);
+        }else {
+            button.setVisibility(View.GONE);
+        }
+    }
+
+    @androidx.databinding.BindingAdapter(value = {"permission","mode"})
+    public static void shouldShowLocateMeButton(ImageButton button, Boolean permission, Mode mode) {
+        if (permission){
+           switch (mode){
+               case LOCATION:
+                   button.setVisibility(View.VISIBLE);
+                   break;
+               case TRACKING:
+                   button.setVisibility(View.GONE);
+                   break;
+           }
+        }else {
+            button.setVisibility(View.GONE);
+        }
+    }
+
+    @androidx.databinding.BindingAdapter(value = {"mode"})
+    public static void shouldShowAnimatedView(DotProgressView progressView, Mode mode) {
+            switch (mode){
+                case LOCATION:
+                    progressView.setVisibility(View.GONE);
+                    break;
+                case TRACKING:
+                    progressView.setVisibility(View.VISIBLE);
+                    break;
+            }
+
+    }
+
+    @androidx.databinding.BindingAdapter(value = {"mode"})
+    public static void customizeToolbar(Toolbar toolbar, Mode mode) {
+        switch (mode){
+            case LOCATION:
+                toolbar.setTitle("Map");
+                break;
+            case TRACKING:
+                toolbar.setTitle("Tracking");
+                break;
+        }
+
+    }
+}
