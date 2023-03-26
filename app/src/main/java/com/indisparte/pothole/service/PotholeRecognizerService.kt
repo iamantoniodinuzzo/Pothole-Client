c1ea2ceaca2d109990ef6389c4a5f3be3aa68b9f
Antonio <tonydinu02@gmail.com>
2023-03-26 17:53:22 +0200

    Update PotholeRecognizerService.kt

--- app/src/main/java/com/indisparte/pothole/service/PotholeRecognizerService.kt
+++ app/src/main/java/com/indisparte/pothole/service/PotholeRecognizerService.kt
@@ -8,80 +8,62 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.IBinder
 import android.util.Log
-import android.widget.Toast
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import com.indisparte.pothole.di.component.PotholeApplication
-import com.indisparte.pothole.service.PotholeRecognizerService
 import com.indisparte.pothole.util.Constant
 
 /**
  * @author Antonio Di Nuzzo (Indisparte)
  */
 class PotholeRecognizerService : Service(), SensorEventListener {
-    private var sensorManager: SensorManager? = null
-    private var accelerometer: Sensor? = null
-    private var gravity: Sensor? = null
-    private var x_accel = 0.0
-    private var y_accel = 0.0
-    private var z_accel = 0.0
-    private var x_gravity = 0.0
-    private var y_gravity = 0.0
-    private var z_gravity = 0.0
-    private var lastEvent = System.currentTimeMillis()
-    private var mThreshold = 0.0
-    override fun onBind(intent: Intent): IBinder? {
-        throw UnsupportedOperationException("Not yet implemented")
+    private lateinit var sensorManager: SensorManager
+    private lateinit var accelerometer: Sensor
+    private lateinit var gravity: Sensor
+    private var lastEventTime :Long = 0L
+    private var threshold = 0.0
+    private var xAccel = 0.0
+    private var yAccel = 0.0
+    private var zAccel = 0.0
+    private var xGravity = 0.0
+    private var yGravity = 0.0
+    private var zGravity = 0.0
+
+    override fun onBind(intent: Intent): IBinder? =null
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent == null) {
+            stopSelf()
+            return START_NOT_STICKY
+        }
+
+        threshold = intent.getDoubleExtra("threshold", Constant.DEFAULT_ACCELERATION_THRESHOLD)
+        Log.d(TAG, "onStartCommand: received threshold ($threshold)")
+
+        when (intent.action) {
+            Constant.ACTION_START_POTHOLE_SERVICE -> startPotholeRecognizerService()
+            Constant.ACTION_STOP_POTHOLE_SERVICE -> stopPotholeRecognizerService()
+        }
+
+        return START_NOT_STICKY
     }
 
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        mThreshold = intent.getDoubleExtra("threshold", Constant.DEFAULT_ACCELERATION_THRESHOLD)
-        Log.d(TAG, "onStartCommand: received threshold ($mThreshold)")
-        val action = intent.action
-        if (action != null) {
-            if (action == Constant.ACTION_START_POTHOLE_SERVICE) {
-                Log.d(TAG, "onStartCommand: start pothole service")
-                startPotholeRecogniserService()
-            } else if (action == Constant.ACTION_STOP_POTHOLE_SERVICE) {
-                Log.d(TAG, "onStartCommand: stop location service")
-                stopPotholeRecognizerService()
-            }
-        }
-        return super.onStartCommand(intent, flags, startId)
-    }
-
-    private fun registerSensor(sensor: Sensor?, success_message: String, error_message: String) {
-        if (sensor != null) {
-            sensorManager!!.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
-            Log.d(TAG, success_message)
-        } else {
-            Log.e(TAG, error_message)
-            Toast.makeText(PotholeApplication.getContext(), error_message, Toast.LENGTH_SHORT)
-                .show()
-        }
+    private fun registerSensor(sensor: Sensor, successMessage: String) {
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        Log.d(TAG, successMessage)
     }
 
     private fun initSensors() {
         Log.d(TAG, "initSensors: Initializing sensors")
-        sensorManager =
-            PotholeApplication.getContext().getSystemService(SENSOR_SERVICE) as SensorManager
-        accelerometer = sensorManager!!.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
-        gravity = sensorManager!!.getDefaultSensor(Sensor.TYPE_GRAVITY)
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) ?: error("Acceleration sensor not found")
+        gravity = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY) ?: error("Gravity sensor not found")
     }
 
-    private fun startPotholeRecogniserService() {
+    private fun startPotholeRecognizerService() {
         try {
             Log.d(TAG, "Service started!")
             initSensors()
-            registerSensor(
-                accelerometer,
-                "Acceleration sensor initialized",
-                "Acceleration sensor not found"
-            )
-            registerSensor(
-                gravity,
-                "Gravity sensor initialized",
-                "Gravity sensor not found"
-            )
+            registerSensor(accelerometer, "Acceleration sensor initialized")
+            registerSensor(gravity, "Gravity sensor initialized")
         } catch (e: Exception) {
             Log.e(TAG, "PotholeSensorListener start: Exception->" + e.message)
             e.printStackTrace()
@@ -97,62 +79,58 @@ class PotholeRecognizerService : Service(), SensorEventListener {
 
     private fun unregisterSensor() {
         Log.d(TAG, "Service stopped!")
-        sensorManager!!.unregisterListener(this, accelerometer)
-        sensorManager!!.unregisterListener(this, gravity)
+        sensorManager.unregisterListener(this)
     }
 
     override fun onSensorChanged(event: SensorEvent) {
-        val sensor = event.sensor
-        getAxisAcceleration(event, sensor)
-        getAxisGravity(event, sensor)
-        val deltaZ = Math.abs(verticalAccel)
-        if (deltaZ > mThreshold) {
+        when (event.sensor.type) {
+            Sensor.TYPE_LINEAR_ACCELERATION -> getAxisAcceleration(event)
+            Sensor.TYPE_GRAVITY -> getAxisGravity(event)
+        }
 
-            /*
-            This block checks if at least MIN_SEC_FOR_ANOTHER_EVENT_REGISTRATION
-             seconds have passed since the last event
-             */
-            val newEvent = System.currentTimeMillis()
-            val secondPassed = (newEvent - lastEvent) / 1000
-            if (secondPassed < MIN_SEC_FOR_ANOTHER_EVENT_REGISTRATION) return
-            lastEvent = newEvent
+        val deltaZ = calculateDeltaZ()
+        if (deltaZ > threshold && isEventRecentEnough()) {
+            lastEventTime = System.currentTimeMillis()
             Log.d(TAG, "onSensorChanged: found pothole with deltaZ = $deltaZ")
             onPotholeFound(deltaZ)
         }
     }
 
+    private fun isEventRecentEnough(): Boolean {
+        val timeSinceLastEvent = System.currentTimeMillis() - lastEventTime
+        return timeSinceLastEvent > MIN_TIME_BETWEEN_EVENTS_MILLIS
+    }
+
     private fun onPotholeFound(deltaZ: Double) {
-        //Notify anyone listening for broadcasts about the new Pothole
         val intent = Intent(Constant.ACTION_BROADCAST)
         intent.putExtra(Constant.EXTRA_DELTA_Z, deltaZ)
         LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(intent)
     }
 
-    private val verticalAccel: Double
-        get() = x_accel * x_gravity / GRAVITY +
-                y_accel * y_gravity / GRAVITY +
-                z_accel * z_gravity / GRAVITY
-
-    private fun getAxisGravity(event: SensorEvent, sensor: Sensor) {
-        if (sensor.type == Sensor.TYPE_GRAVITY) {
-            x_gravity = event.values[0].toDouble()
-            y_gravity = event.values[1].toDouble()
-            z_gravity = event.values[2].toDouble()
-        }
+    private fun calculateDeltaZ(): Double {
+        val verticalAccel = xAccel * xGravity / GRAVITY +
+                yAccel * yGravity / GRAVITY +
+                zAccel * zGravity / GRAVITY
+        return Math.abs(verticalAccel)
     }
 
-    private fun getAxisAcceleration(event: SensorEvent, sensor: Sensor) {
-        if (sensor.type == Sensor.TYPE_LINEAR_ACCELERATION) {
-            x_accel = event.values[0].toDouble()
-            y_accel = event.values[1].toDouble()
-            z_accel = event.values[2].toDouble()
-        }
+    private fun getAxisGravity(event: SensorEvent) {
+            xGravity = event.values[0].toDouble()
+            yGravity = event.values[1].toDouble()
+            zGravity = event.values[2].toDouble()
+    }
+
+    private fun getAxisAcceleration(event: SensorEvent) {
+            xAccel = event.values[0].toDouble()
+            yAccel = event.values[1].toDouble()
+            zAccel = event.values[2].toDouble()
+
     }
 
     override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
 
     companion object {
-        private const val MIN_SEC_FOR_ANOTHER_EVENT_REGISTRATION = 2
+        private const val MIN_TIME_BETWEEN_EVENTS_MILLIS = 2 // seconds
         private const val GRAVITY = 9.18
         private val TAG = PotholeRecognizerService::class.java.simpleName
     }
